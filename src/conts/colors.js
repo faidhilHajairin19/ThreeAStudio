8960cac2e7e01490253654e5724318055818a5ce
faidhilHajairin19 <hajairinfaidhil19@gmail.com>
2025-04-30 00:34:44 +0800

    2

---
+++ src/conts/colors.js
@@ -0,0 +1,14 @@
+const COLORS = {
+    white: '#fff',
+    black: '#000',
+    blue: '#5D5FEE',
+    grey: '#BABBC3',
+    grey2: '#6C757D',
+    lightGrey: '#EEEEEE',
+    light: '#F3F4FB',
+    darkBlue: '#7978B5',
+    red: 'red',
+    lightBlue : '#0782F9',
+    green:'#32b768'
+  };
+  export default COLORS;
---
+++ src/conts/detail.js
@@ -0,0 +1,168 @@
+
+export const GroupPelajar1 = {
+    maxOrang: 'Max 10 orang',
+    harga: 'Rp. 258.000',
+    outfit: 'Salah satu outfit wajib seragam sekolah/almamater yaa',
+    desc1: '1 Background set, Max. 2 outfit',
+    desc2: '1 file pilihan edit dan 4 file pilihan original',
+    desc3: '1 file edit dicetak ukuran 4R sesuai jumlah orang yang foto',
+    nb1: '25K (Per 1 file original)',
+    nb2: '50K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GroupPelajar2 = {
+    maxOrang: 'Max 20 orang',
+    harga: 'Rp. 368.000',
+    outfit: 'Salah satu outfit wajib seragam sekolah/almamater yaa',
+    desc1: '1 Background set, Max. 2 outfit',
+    desc2: '1 file pilihan edit dan 4 file pilihan original',
+    desc3: '1 file edit dicetak ukuran 4R sesuai jumlah orang yang foto',
+    nb1: '25K (Per 1 file original)',
+    nb2: '50K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GroupPelajar3 = {
+    maxOrang: 'Max 30 orang',
+    harga: 'Rp. 498.000',
+    outfit: 'Salah satu outfit wajib seragam sekolah/almamater yaa',
+    desc1: '1 Background set, Max. 2 outfit',
+    desc2: '1 file pilihan edit dan 4 file pilihan original',
+    desc3: '1 file edit dicetak ukuran 4R sesuai jumlah orang yang foto',
+    nb1: '25K (Per 1 file original)',
+    nb2: '50K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GroupPelajar4 = {
+    maxOrang: 'Max 40 orang',
+    harga: 'Rp. 628.000',
+    outfit: 'Salah satu outfit wajib seragam sekolah/almamater yaa',
+    desc1: '1 Background set, Max. 2 outfit',
+    desc2: '1 file pilihan edit dan 4 file pilihan original',
+    desc3: '1 file edit dicetak ukuran 4R sesuai jumlah orang yang foto',
+    nb1: '25K (Per 1 file original)',
+    nb2: '50K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+
+export const GroupUmum1 = {
+    maxOrang: 'Max 10 orang',
+    harga: 'Rp. 298.000',
+    outfit: 'Tidak ada ketentuan outfit',
+    desc1: '1 Background Set, Max. 2 outfit',
+    desc2:  '3 File pilihan edit',
+    desc3: 'Salah satu file edit dicetak Ukuran 4R sesuai jumlah orang yang foto',
+    nb1: '40K (Per 1 file original)',
+    nb2: '70K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const FamilyStandart = {
+    maxOrang: 'Max 5 orang',
+    harga: 'Rp. 429.000',
+    outfit: 'Tidak ada ketentuan outfit',
+    desc1: '1 Background Set, Max. 1 Kostum',
+    desc2:  '3 File pilihan edit',
+    desc3: '3 file edit dicetak Ukuran 10R Jumbo',
+    nb1: '40K (Per 1 file original)',
+    nb2: '70K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const FamilyBronze = {
+    maxOrang: 'Max 8 orang',
+    harga: 'Rp. 698.000',
+    outfit: 'Tidak ada ketentuan outfit',
+    desc1: '1 Background Set, Max. 2 kostum',
+    desc2:  '5 File pilihan edit dicetak 4R',
+    desc3: 'Free 1x Pembesaran 12R Jumbo',
+    nb1: '40K (Per 1 file original)',
+    nb2: '70K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const FamilySilver = {
+    maxOrang: 'Max 12 orang',
+    harga: 'Rp. 949.000',
+    outfit: 'Tidak ada ketentuan outfit',
+    desc1: '2 Background Set, Max. 2 kostum',
+    desc2:  '8 File pilihan edit dicetak 4R',
+    desc3: 'Free 1x Pembesaran 18R',
+    nb1: '40K (Per 1 file original)',
+    nb2: '70K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const FamilyGold = {
+    maxOrang: 'Max 20 orang',
+    harga: 'Rp. 1.449.000',
+    outfit: 'Tidak ada ketentuan outfit',
+    desc1: '2 Background Set, Max. 2 kostum',
+    desc2:  '12 File pilihan edit dicetak 4R',
+    desc3: 'Free 1x Pembesaran 20R Jumbo',
+    nb1: '40K (Per 1 file original)',
+    nb2: '70K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+
+export const GradSt1 = {
+    maxOrang: 'Max 5 orang',
+    harga: 'Rp. 449.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '1 Background Set, Max. 1 kostum',
+    desc2:  '5 File pilihan edit dicetak 4R',
+    desc3: '',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GradSt2 = {
+    maxOrang: 'Max 10 orang',
+    harga: 'Rp. 589.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '1 Background Set, Max. 1 kostum',
+    desc2:  '5 File pilihan edit dicetak 4R',
+    desc3: 'Free 1x Pembesaran 18R',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GradMd1 = {
+    maxOrang: 'Max 10 orang',
+    harga: 'Rp. 1,049.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '2 Background Set, Max. 2 kostum',
+    desc2:  '5 File pilihan edit dicetak 4R dan 3 File Pilihan Edit Cetak 10RJ',
+    desc3: 'Free 2x Pembesaran 18R',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GradMd2 = {
+    maxOrang: 'Max 12 orang',
+    harga: 'Rp. 1.299.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '2 Background Set, Max. 2 kostum',
+    desc2:  '7 File pilihan edit dicetak 4R dan 3 File Pilihan Edit Cetak 10RJ',
+    desc3: 'Free 2x Pembesaran 20R',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GradPrem1 = {
+    maxOrang: 'Max 8 orang || ALL FILE ORI',
+    harga: 'Rp. 1.699.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '3 Background Set, Max. 3 kostum',
+    desc2:  '10 File pilihan edit dicetak 4R dan 3 File Pilihan Edit Cetak 10RJ',
+    desc3: 'Free 1x Pembesaran 20R Jumbo',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
+export const GradPrem2 = {
+    maxOrang: 'Max 12 orang || ALL FILE ORI',
+    harga: 'Rp. 2.449.000',
+    outfit: 'Salah Satu Foto Memakai Toga',
+    desc1: '3 Background Set, Max. 3 kostum',
+    desc2:  '15 File pilihan edit dicetak 4R dan 3 File Pilihan Edit Cetak 10RJ',
+    desc3: 'Free 1x Pembesaran 20R Jumbo + Frame',
+    nb1: '45K (Per 1 file original)',
+    nb2: '75K (Per 1 file edit tanpa cetak)',
+    nb3: 'Penambahan orang: 25K/kepala'
+}
